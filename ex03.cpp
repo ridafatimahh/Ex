480baab9f1233d21b81e65a65823506f010361b3
ridafatimahh <164626276+ridafatimahh@users.noreply.github.com>
2024-10-26 17:08:27 +0500

    Create ex03.cpp

---
+++ ex03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+static void print_digits() {
+	int i;
+	 for (i = 0; i < 11; i++) {
+		cout << i << " ";
+	};
+}
+
+int main() {
+
+	print_digits();
+
+
+
+	return 0;
+}
