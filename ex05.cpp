368dfd006057ce395277742d9c30b3b5dda18fbb
ridafatimahh <164626276+ridafatimahh@users.noreply.github.com>
2024-10-26 17:40:29 +0500

    Update ex05.cpp

--- ex05.cpp
+++ ex05.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+static void print_comb() {
+    for (int i = 0; i <= 7; i++) {
+        for (int j = i + 1; j <= 8; j++) {
+            for (int k = j + 1; k <= 9; k++) {
+                cout << i << j << k;
+                
+                    cout << ", " ;
+                }
+            }
+        }
+    }
+
+
+
+
+int main() {
+	
+    print_comb();
+
+return 0;
+}
