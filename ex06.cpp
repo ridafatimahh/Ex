864400783172fe8f75c4b6d2341a334ca3970229
ridafatimahh <164626276+ridafatimahh@users.noreply.github.com>
2024-10-26 17:47:47 +0500

    Update ex06.cpp

--- ex06.cpp
+++ ex06.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+static void print_comb2() {
+    for (int i = 0; i <= 98; i++) {
+        for (int j = i + 1; j <= 99; j++) {
+          if (i < 10) cout << "0";
+            cout << i << " ";
+
+          if (j < 10) cout << "0";
+            cout << j;
+
+         
+            if (!(i == 98 && j == 99)) {
+                cout << ", " ;
+            }
+        }
+    }
+   
+}
+
+int main() {
+
+    print_comb2();
+
+    return 0;
+}
