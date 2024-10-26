45c9310defbbadd2f55580bedc2a0fa0b6473d7e
ridafatimahh <164626276+ridafatimahh@users.noreply.github.com>
2024-10-26 17:03:41 +0500

    Create ex02.cpp

---
+++ ex02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+static void print_alphabet() {
+	char X;
+	 for (X = 'z'; X >= 'a'; X--) {
+		cout << X << " ";
+	};
+}
+
+int main() {
+
+	print_alphabet();
+
+
+
+	return 0;
+}
