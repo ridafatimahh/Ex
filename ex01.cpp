904bb7b9af6bd11666537d19c633ec8cc3325078
ridafatimahh <164626276+ridafatimahh@users.noreply.github.com>
2024-10-26 16:59:18 +0500

    Create ex01.cpp

---
+++ ex01.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+using namespace std;
+
+static void print_alphabet() {
+	char X;
+	for (X = 'a'; X <= 'z'; X++) {
+		cout << X << endl;
+	}
+}
+
+int main() {
+
+	print_alphabet();
+
+
+
+	return 0;
+}
